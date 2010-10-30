22a586d4d256516ee60cce945eea7a8f39fc3829
Jens W. Klein <jens@bluedynamics.com>
2010-10-31 00:13:12 +0100

    so, base_edit renders, now the jquery part

--- src/collective/dynatree/atwidget.py
+++ src/collective/dynatree/atwidget.py
@@ -1,10 +1,10 @@
 from z3c.json.converter import JSONWriter
+from AccessControl import ClassSecurityInfo
 from Products.Five.browser import BrowserView
 from Products.Archetypes.Widget import TypesWidget
 from Products.Archetypes.Registry import registerWidget
 from Products.Archetypes.interfaces import IVocabulary
 from Products.Archetypes.utils import OrderedDict
-
 
 class DynatreeWidgetMacros(BrowserView):
     """
@@ -36,10 +36,19 @@ class DynatreeWidget(TypesWidget):
                         'minExpandLevel': 1,
                         'rootvisible': False,
                         'autocollapse': False})
+    
+    security = ClassSecurityInfo()
+    
+    def dynatreeParameters(self):
+        result = [('%s,%s' % (_, self._properties[_])) 
+                  for _ in ['select', 'minExpandLevel', 'rootvisible', 
+                            'autocollapse']]        
+        return ';'.join(result)
 
 registerWidget(DynatreeWidget,
                title='Dynatree',
                description=('Renders a tree with selected items '
                             'Allows selection of more items via pop up'),
-               used_for=('Products.Archetypes.Field.LinesField',)
+               used_for=('Products.Archetypes.Field.StringField',
+                         'Products.Archetypes.Field.LinesField')
 )
