089f913b146f23d93db3b481acf41fd1fe9d4c75
Wolfgang Thomas <thomas@syslab.com>
2011-12-22 13:50:26 +0100

    remove trailing spaces

--- src/collective/dynatree/widget.py
+++ src/collective/dynatree/widget.py
@@ -10,7 +10,7 @@ import interfaces
 from Products.ATVocabularyManager import NamedVocabulary
 
 class FieldVocabDynatreeJsonView(BrowserView):
-    
+
     def __call__(self):
         fieldname = self.request.get('fieldname')
         #field = self.context.schema[fieldname]
@@ -18,10 +18,10 @@ class FieldVocabDynatreeJsonView(BrowserView):
         atv = NamedVocabulary('staralliance-productcategories')
         tree = atv.getVocabularyDict(self.context)
         selected = self.request.get('selected', []).split('|')
-        return JSONWriter().write(dict2dynatree(tree, selected, 
+        return JSONWriter().write(dict2dynatree(tree, selected,
                                                 True,
                                                 False))
-    
+
 class DynatreeWidget(z3c.form.browser.text.TextWidget):
     """
     A text field widget with a dynatree javascript vocabulary to determine the
@@ -36,27 +36,27 @@ class DynatreeWidget(z3c.form.browser.text.TextWidget):
     leafsOnly = True
     showKey = False
     atvocabulary = None
-    
+
     def dynatree_parameters(self):
-        result = [('%s,%s' % (parameter, getattr(self, parameter))) 
+        result = [('%s,%s' % (parameter, getattr(self, parameter)))
                   for parameter in ['selectMode',
                                     'minExpandLevel',
-                                    'rootVisible', 
+                                    'rootVisible',
                                     'autoCollapse']]
         result.append('title,%s' % self.label)
         return '/'.join(result)
-    
+
     def dynatree_value(self, value):
         if isinstance(value, basestring):
             return value
         if isinstance(value, (tuple, list)):
             return '|'.join(value)
-        return ''     
+        return ''
 
 @zope.component.adapter(zope.schema.TextLine, z3c.form.interfaces.IFormLayer)
 @zope.interface.implementer(z3c.form.interfaces.IFieldWidget)
 def DynatreeFieldWidget(field, request):
-    """ IFieldWidget factory for DynatreeWidget 
+    """ IFieldWidget factory for DynatreeWidget
     """
     return z3c.form.widget.FieldWidget(field, DynatreeWidget(request))
 
