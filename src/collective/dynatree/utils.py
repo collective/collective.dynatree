6b07b02ab17101386dd98a83926001bcd3cd9cb5
JC Brand <jc@opkode.com>
2012-01-19 17:05:51 +0200

    Rename dict2dynatree to atvocabularydict2dynatree and reuse original for TreeVocabulary vocabs.

--- src/collective/dynatree/utils.py
+++ src/collective/dynatree/utils.py
@@ -1,8 +1,41 @@
 from Products.Archetypes.interfaces import IVocabulary
 from Products.Archetypes.utils import OrderedDict
 
+def dict2dynatree(input_dict, selected, only_leaves, showKey=False):
+    """
+    Recursively parse the dictionary as we get it from the
+    IVocabulary, and transform it to a a dictionary as needed for
+    dynatree
+    input_dict:
+        dictionary as provided by getVocabularyDict from 
+        Products.ATVocabularyManager
+    selected:
+        List of keys that should be preselected
+    only_leaves:
+        Whether only leafes should be selectable or also tree nodes
+    showKey:
+        Whether the title should start with the key or not
+    """
+    if not input_dict:
+        return []
+    retval = []
+    for key in input_dict.keys():
+        children = dict2dynatree(input_dict[key], selected, only_leaves, showKey)
 
-def dict2dynatree(input_dict, selected, leafsOnly, showKey=False):
+        new_item = {}  # we have to have boolItems
+        new_item['title'] = key.value
+        new_item['key'] = key.token
+        new_item['children'] = children
+        new_item['select'] = key.token in selected
+        new_item['isFolder'] = bool(children)
+        new_item['hideCheckbox'] = bool(children) and only_leaves
+        new_item['expand'] = (key.token in selected or
+                              isSomethingSelectedInChildren(children,
+                                                            selected))
+        retval.append(new_item)
+    return retval
+
+def atvocabularydict2dynatree(input_dict, selected, leafsOnly, showKey=False):
     """
     Recursively parse the dictionary as we get it from the
     IVocabulary, and transform it to a a dictionary as needed for
