d32733a0674d9c58d27963c3949ce1a962f706b0
JC Brand <jc@opkode.com>
2012-01-19 17:07:10 +0200

    Subclass from ISelectWidget, not ITextWidget.

--- src/collective/dynatree/interfaces.py
+++ src/collective/dynatree/interfaces.py
@@ -1,5 +1,5 @@
 import z3c.form.interfaces
 
-class IDynatreeWidget(z3c.form.interfaces.ITextWidget): 
-    """A Dynatree widget for textfields
+class IDynatreeWidget(z3c.form.interfaces.ISelectWidget): 
+    """ A Dynatree widget 
     """
