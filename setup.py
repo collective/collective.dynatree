ed7b44d834b146e17082a0135d5ad2d4904e2725
Peter Holzer <hpeter@agitator.com>
2014-10-30 10:57:17 +0000

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0'
+version = '2.1.dev0'
 shortdesc = 'jquery.dynatree.js integration for Plone'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc = open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
