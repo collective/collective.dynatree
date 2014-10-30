9a33bcc139f718edd0184d16e2dc621528be2458
Peter Holzer <hpeter@agitator.com>
2014-10-30 10:55:07 +0000

    added changes.rst

---
+++ CHANGES.rst
@@ -0,0 +1,87 @@
+Changes
+=======
+
+----------------
+2.0 (Unreleased)
+----------------
+
+- Several enhancements and minor bug fixes.
+  [regebro]
+
+- Bunch of fixes for z3cform widget, example types for dx, integration with
+  fixed tree vocabularies of ``collective.vdexvocabulary`` and more polishing.
+  Updated to latest ``jquery.dynatree``. Some renamings (attention, imports are
+  changing slightly).
+  [jensens, bennyboy, 2014-01-27]
+
+- Add a z3c.form dynatree widget.
+  [jbeyers, thomas_w, jcbrand 2012-02-08]
+
+------------------
+1.3.4 (2011-03-31)
+------------------
+
+- for some cases we need to explicit iterate over .keys() - thanks to Jess Hix
+  for the patch, jensens 2011-03-31
+
+------------------
+1.3.3 (2011-03-14)
+------------------
+
+- fixed JS bug with f****g IE. Ported solution used by hpeter at
+  ``yafowil.widget.dynatree`` witha regexp instead of trim, jensens 2011-03-14
+
+------------------
+1.3.2 (2011-03-08)
+------------------
+
+- fixed bug: css-registry merges css, so paths to skin were no longer relative.
+  Adding the resource part helps here. jensens 2011-03-08
+
+------------------
+1.3.1 (2011-02-18)
+------------------
+
+- fixed bug: ``required`` on multi-selection did not work. jensens 2011-02-18
+
+- added ``showKey`` property to at-widget to show terms key in front of the
+  value. hpeter, jensens, 2010-01-18
+
+----------------
+1.3 (2011-01-19)
+----------------
+
+- upgraded jquery.dynatree from upstream to version 1.0.3. jensens 2011-01-19
+
+- added ``showKey`` property to at-widget to show terms key in front of the value.
+  hpeter, jensens, 2011-01-18
+
+------------------
+1.2.1 (2010-12-03)
+------------------
+
+- fighting with MANIFEST.in, to much was excluded and egg release broken.
+  should now be better. jensens 2010-12-03
+
+----------------
+1.2 (2010-12-02)
+----------------
+
+- after submit and validation error keep the previous selected values.
+  jensens, 2010-12-02
+
+----------------
+1.1 (2010-11-29)
+----------------
+
+- add MANIFEST.in, so ``*.rst`` gets included in the egg.
+  jensens, 2010-11-29
+
+- make dict2dynatree more robust after report by Rigel Di Scala,
+  jensens, 2010-11-29
+
+----------------
+1.0 (2010-11-22)
+----------------
+
+- Make it work (jensens, hpeter)
--- setup.py
+++ setup.py
@@ -4,46 +4,49 @@ import os
 version = "2.0dev"
 shortdesc = 'jquery.dynatree.js integration for Plone'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc = open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
-setup(name='collective.dynatree',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-      ],
-      keywords='jquery dynatree Zope Plone Archetypes Dexterity Widget tree vocabulary',
-      author='BlueDynamics Alliance, et al.',
-      author_email='dev@bluedynamics.com',
-      url=u'https://github.com/collective/collective.dynatree',
-      license='GNU General Public Licence',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.schema >= 4.1dev',
-          'Plone',
-          'plone.behavior',
-          'collective.js.jqueryui',
-          'collective.js.backbone',
-      ],
-      extras_require=dict(
-          at_example=['Products.ATVocabularyManager'],
-          dx_example=['plone.app.contenttypes',
-                      'collective.vdexvocabulary'],
-          test=['ipdb', 'interlude', ],
-      ),
-      entry_points={
-          'z3c.autoinclude.plugin': 'target = plone',
-      },
+setup(
+    name='collective.dynatree',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='jquery dynatree Zope Plone Archetypes Dexterity Widget tree '
+             'vocabulary',
+    author='BlueDynamics Alliance, et al.',
+    author_email='dev@bluedynamics.com',
+    url=u'https://github.com/collective/collective.dynatree',
+    license='GNU General Public Licence',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.schema >= 4.1dev',
+        'Plone',
+        'plone.behavior',
+        'collective.js.jqueryui',
+        'collective.js.backbone',
+    ],
+    extras_require=dict(
+        at_example=['Products.ATVocabularyManager'],
+        dx_example=['plone.app.contenttypes',
+                    'collective.vdexvocabulary'],
+        test=['ipdb', 'interlude', ],
+    ),
+    entry_points={
+        'z3c.autoinclude.plugin': 'target = plone',
+    },
 )
